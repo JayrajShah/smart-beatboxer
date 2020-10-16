c56ca4262f2caaab4f67c8d70046da71b51cc986
Jayraj Shah <jayrajcshah@gmail.com>
2020-10-16 13:25:08 +0530

    React-init

---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./test.css";
+
+const Test = () => {
+  return (
+    <div className="App">
+      <div>HEY this is test</div>
+    </div>
+  );
+};
+
+export default Test;
